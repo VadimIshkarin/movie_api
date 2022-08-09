3c02bfc60697082c93dbc7e2a0ea2c58975bccb8
VadimIshkarin <theishkarin@gmail.com>
2022-08-09 13:22:30 -0400

    Added changes to index.js file (cors)

--- index.js
+++ index.js
@@ -3,12 +3,26 @@ const express = require("express"),
   uuid = require("uuid");
 //Using CORS
 const cors = require("cors");
-app.use(cors());
 
 const { check, validationResult } = require("express-validator");
 
 //A list of allowed domains within the variable allowedOrigins
-// let allowedOrigins = ["http://localhost:8080", "http://testsite.com"];
+let allowedOrigins = ["http://localhost:8080", "http://localhost:1234"];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message =
+          "The CORS policy for this application doesn’t allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 const morgan = require("morgan");
 const app = express();
