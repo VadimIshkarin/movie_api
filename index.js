3f22558f5b61b4dd571126cd5007ccdda42c38fb
VadimIshkarin <theishkarin@gmail.com>
2022-11-17 13:15:33 -0500

    Added myFlix-Angular-client gh-pages to a list of allowed domains

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ let allowedOrigins = [
   "http://localhost:1234",
   "http://localhost:4200",
   "https://myflix-client.vercel.app",
+  "https://vadimishkarin.github.io/myFlix-Angular-client/",
 ];
 
 const morgan = require("morgan");
